bf614d53994ae2fa74560933f1a39e246062efa0
jesusmplana <jesusmpcampos@gmail.com>
2025-02-06 10:38:45 +0100

    Implementar estructura básica de servicios y controladores, renombrar atributos de usuario y actualizar formularios de inicio de sesión(Las lineas estan comentadas hasta que se solucione el problema del despliegue)

--- src/main/java/com/proyectoTransversal/controller/IndexController.java
+++ src/main/java/com/proyectoTransversal/controller/IndexController.java
@@ -1,13 +1,41 @@
 package com.proyectoTransversal.controller;
 
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+
+// import com.proyectoTransversal.entity.UsuarioEntity;
+// import com.proyectoTransversal.services.IndexService;
+
 
 @Controller
 public class IndexController {
 	
+	// @Autowired
+	// private IndexService indexService;
+
 	@GetMapping("/")
 	public String indexController() {
 		return "index";
 	}
+
+	// @GetMapping("/comprobarDatos")
+	// public String iniciarSesion(@RequestParam("nombreUsuario") String nombreUsuario, @RequestParam("pass") String pass, Model model){
+	// 	UsuarioEntity usuario = indexService.buscarPorUsuario(nombreUsuario);
+
+	// 	if(usuario != null){
+	// 		if(usuario.getPass().equals(pass)){
+	// 			if(usuario.getUsuarioVip() == true){
+	// 				return "lobbyVip";
+	// 			} else {
+	// 				return "lobby";
+	// 			}
+	// 		} else{
+	// 			model.addAttribute("mensajeErro", "Contraseña incorrecta");
+	// 		}
+	// 	}
+	// 	return "index";
+	// }
 }
