323536bc058be4f463df19951e0ec87f563970ba
DiegoArroyo04 <diegoarroyogonzalez04@gmail.com>
2025-02-06 09:18:05 +0100

    espacios

--- src/main/java/com/proyectoTransversal/controller/CrashController.java
+++ src/main/java/com/proyectoTransversal/controller/CrashController.java
@@ -1,18 +1,14 @@
 package com.proyectoTransversal.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
 @Controller
 public class CrashController {
-	
+
 	@GetMapping("/juegoCrash")
 	public String juegoCrash() {
 		return "crashGame";
-	}	
-	
-	
+	}
+
 }
