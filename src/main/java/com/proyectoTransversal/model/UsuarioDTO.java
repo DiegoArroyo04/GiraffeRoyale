b1040733bae8d3de540e78c8726d992901414b57
DiegoArroyo04 <diegoarroyogonzalez04@gmail.com>
2025-02-14 19:51:04 +0100

    pequeños avances validacion de campos vip

--- src/main/java/com/proyectoTransversal/model/UsuarioDTO.java
+++ src/main/java/com/proyectoTransversal/model/UsuarioDTO.java
@@ -3,6 +3,10 @@ package com.proyectoTransversal.model;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
 public class UsuarioDTO {
 
     private String dni;
@@ -14,18 +18,73 @@ public class UsuarioDTO {
     private Date fechaNacimiento;
     private BigDecimal presupuesto;
     private Boolean usuarioVip;
-    
-    //CONSTRUCTOR PARA ACTUALIZAR SALDO
-    public UsuarioDTO(String dni, BigDecimal presupuesto) {
-		this.dni = dni;
-		this.presupuesto = presupuesto;
-	}
-    
-	public UsuarioDTO() {
-		
-	}
+    private String pass;
+    private String tarjetaCredito;
+    private Date fechaExpiracionTarjeta;
+    private Integer cvcTarjeta;
+    private String cuentaBancaria;
+    private String titular;
 
-	// Getters y Setters
+    // CONSTRUCTOR USUARIO NO VIP
+    public UsuarioDTO(String dni, String nombre, String apellidos, String nombreUsuario, String email, String telefono,
+            Date fechaNacimiento, Boolean usuarioVip, String pass) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.nombreUsuario = nombreUsuario;
+        this.email = email;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+        this.usuarioVip = usuarioVip;
+        this.pass = pass;
+    }
+
+    // CONSTRUCTOR PARA USUARIO VIP CON TARJETA BANCARIA
+    public UsuarioDTO(String dni, String nombre, String apellidos, String nombreUsuario, String email, String telefono,
+            Date fechaNacimiento, Boolean usuarioVip, String pass, String cuentaBancaria, String titular) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.nombreUsuario = nombreUsuario;
+        this.email = email;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+        this.usuarioVip = usuarioVip;
+        this.pass = pass;
+        this.cuentaBancaria = cuentaBancaria;
+        this.titular = titular;
+    }
+
+    // CONSTRUCTOR PARA USUARIO VIP CON TARJETA
+    public UsuarioDTO(String dni, String nombre, String apellidos, String nombreUsuario, String email, String telefono,
+            Date fechaNacimiento, Boolean usuarioVip, String pass, String tarjetaCredito, Date fechaExpiracionTarjeta,
+            Integer cvcTarjeta, String titular) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.nombreUsuario = nombreUsuario;
+        this.email = email;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+        this.usuarioVip = usuarioVip;
+        this.pass = pass;
+        this.tarjetaCredito = tarjetaCredito;
+        this.fechaExpiracionTarjeta = fechaExpiracionTarjeta;
+        this.cvcTarjeta = cvcTarjeta;
+        this.titular = titular;
+    }
+
+    // CONSTRUCTOR PARA ACTUALIZAR SALDO
+    public UsuarioDTO(String dni, BigDecimal presupuesto) {
+        this.dni = dni;
+        this.presupuesto = presupuesto;
+    }
+
+    public UsuarioDTO() {
+
+    }
+
+    // Getters y Setters
     public String getDni() {
         return dni;
     }
@@ -97,4 +156,53 @@ public class UsuarioDTO {
     public void setUsuarioVip(Boolean usuarioVip) {
         this.usuarioVip = usuarioVip;
     }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public String getTarjetaCredito() {
+        return tarjetaCredito;
+    }
+
+    public Date getFechaExpiracionTarjeta() {
+        return fechaExpiracionTarjeta;
+    }
+
+    public Integer getCvcTarjeta() {
+        return cvcTarjeta;
+    }
+
+    public String getCuentaBancaria() {
+        return cuentaBancaria;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public void setTarjetaCredito(String tarjetaCredito) {
+        this.tarjetaCredito = tarjetaCredito;
+    }
+
+    public void setFechaExpiracionTarjeta(Date fechaExpiracionTarjeta) {
+        this.fechaExpiracionTarjeta = fechaExpiracionTarjeta;
+    }
+
+    public void setCvcTarjeta(Integer cvcTarjeta) {
+        this.cvcTarjeta = cvcTarjeta;
+    }
+
+    public void setCuentaBancaria(String cuentaBancaria) {
+        this.cuentaBancaria = cuentaBancaria;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
 }
