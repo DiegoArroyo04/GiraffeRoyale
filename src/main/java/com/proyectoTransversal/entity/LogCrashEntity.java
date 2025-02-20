8d47596f0248ccb3a4b8c363a810d97f70426755
jesusmplana <jesusmpcampos@gmail.com>
2025-02-20 23:50:44 +0100

    Elimina importaciones innecesarias  y ajusta la transición en el CSS de tragaperras.

--- src/main/java/com/proyectoTransversal/entity/LogCrashEntity.java
+++ src/main/java/com/proyectoTransversal/entity/LogCrashEntity.java
@@ -2,7 +2,6 @@ package com.proyectoTransversal.entity;
 
 import java.io.Serializable;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
