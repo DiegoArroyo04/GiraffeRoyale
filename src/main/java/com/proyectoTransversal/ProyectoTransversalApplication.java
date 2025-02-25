87157637e865e3bb0508fae0faaff92ae652e0f3
DiegoArroyo04 <diegoarroyogonzalez04@gmail.com>
2025-02-25 10:06:30 +0100

    Despliegue en railway

--- src/main/java/com/proyectoTransversal/ProyectoTransversalApplication.java
+++ src/main/java/com/proyectoTransversal/ProyectoTransversalApplication.java
@@ -2,6 +2,10 @@ package com.proyectoTransversal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProyectoTransversalApplication {
@@ -10,4 +14,18 @@ public class ProyectoTransversalApplication {
 		SpringApplication.run(ProyectoTransversalApplication.class, args);
 	}
 
+	// Configuración para el despliegue en Railway
+	@Configuration
+	public static class Myconfiguration {
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
