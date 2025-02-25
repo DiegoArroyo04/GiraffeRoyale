ed79d8ebcf49f475428c256d789a5b8382f1362f
DiegoArroyo04 <diegoarroyogonzalez04@gmail.com>
2025-02-25 16:52:30 +0100

    revert casino

--- src/main/java/com/proyectoTransversal/ProyectoTransversalApplication.java
+++ src/main/java/com/proyectoTransversal/ProyectoTransversalApplication.java
@@ -2,10 +2,6 @@ package com.proyectoTransversal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProyectoTransversalApplication {
@@ -14,18 +10,4 @@ public class ProyectoTransversalApplication {
 		SpringApplication.run(ProyectoTransversalApplication.class, args);
 	}
 
-	// Configuración para el despliegue en Railway
-	@Configuration
-	public static class Myconfiguration {
-		@Bean
-		public WebMvcConfigurer corsConfigurer() {
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-			};
-		}
-	}
 }
